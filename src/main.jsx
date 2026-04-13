7b037d1678ee18cd911fb433c2eb41365d422813
Johnathan-BauProjects <johnathan@bauprojects.ca>
2026-04-13 08:49:22 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Docket from './docket'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Docket />
+  </React.StrictMode>
+)
